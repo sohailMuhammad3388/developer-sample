24f1bdc0edcecc6e5ef4310d2823f5fb7a72b4c4
sohailMuhammad3388 <144048564+sohailMuhammad3388@users.noreply.github.com>
2023-09-04 12:18:16 -0400

    Update LoginAttemptList.js

--- Frontend/login_app/src/LoginAttemptList.js
+++ Frontend/login_app/src/LoginAttemptList.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./LoginAttemptList.css";
 
+const users = [
+	{ username: 'user1', password: '12344' },
+	{ username: 'user2', password: '12344' },
+];
+export const getByusername = (username) => users.find((user) => user.username === username);
+
 const LoginAttempt = (props) => <li {...props}>{props.children}</li>;
 
 const LoginAttemptList = (props) => (
 	<div className="Attempt-List-Main">
-	 	<p>Recent activity</p>
-	  	<input type="input" placeholder="Filter..." />
+		<p>Recent activity</p>
+		<input type="input" placeholder="Filter..." />
 		<ul className="Attempt-List">
 			<LoginAttempt>TODO</LoginAttempt>
 		</ul>
 	</div>
 );
 
-export default LoginAttemptList;
+export LoginAttemptList, { users }, { getByusername };
