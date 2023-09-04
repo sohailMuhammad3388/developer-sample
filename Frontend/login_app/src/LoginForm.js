a7f32c8ecf7852ca5991653e552cea393b370b30
sohailMuhammad3388 <144048564+sohailMuhammad3388@users.noreply.github.com>
2023-09-04 12:19:18 -0400

    Update LoginForm.js

--- Frontend/login_app/src/LoginForm.js
+++ Frontend/login_app/src/LoginForm.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState} from "react";
 import './LoginForm.css';
 
 const LoginForm = (props) => {
+	const [userName, setUserName] = useState('');
+	const [password, setpassword] = useState('');
+
 	const handleSubmit = (event) =>{
 		event.preventDefault();
 
 		props.onSubmit({
-			login: undefined,
-			password: undefined,
+			login: userName,
+			password: password,
 		});
 	}
 
@@ -15,9 +18,9 @@ const LoginForm = (props) => {
 		<form className="form">
 			<h1>Login</h1>
 			<label htmlFor="name">Name</label>
-			<input type="text" id="name" />
+			<input value={userName} type="text" id="name" />
 			<label htmlFor="password">Password</label>
-			<input type="password" id="password" />
+			<input value={password} type="password" id="password" />
 			<button type="submit" onClick={handleSubmit}>Continue</button>
 		</form>
 	)
