dc7ccf3db30d885562be6ff9188cb0e5c4140967
sohailMuhammad3388 <144048564+sohailMuhammad3388@users.noreply.github.com>
2023-09-04 12:12:52 -0400

    Update Algorithms.cs

--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -4,8 +4,46 @@ namespace DeveloperSample.Algorithms
 {
     public static class Algorithms
     {
-        public static int GetFactorial(int n) => throw new NotImplementedException();
+        public static int GetFactorial(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException($"Provide Non negative number.");
+            }
+            if (n = 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * GetFactorial(n - 1);
 
-        public static string FormatSeparators(params string[] items) => throw new NotImplementedException();
+            }
+        }
+
+        public static string FormatSeparators(params string[] items)
+        {
+            if (items == null)
+            {
+                throw new ArgumentException($"invalied parameter or provide parameter.");
+            }
+            if (items.Length == 0)
+            {
+                return string.Empty;
+            }
+            var result = items[0];
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (i == items.Length - 1)
+                {
+                    result += "and " + items[i];
+                }
+                else
+                {
+                    result += ", " + items[i];
+                }
+            }
+            return result;
+        }
     }
 }
